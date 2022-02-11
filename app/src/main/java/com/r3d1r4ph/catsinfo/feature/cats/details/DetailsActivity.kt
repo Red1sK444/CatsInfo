76c77fa4096a432572ba6618e53c5b4e8d88bfc0
Armen Gevorkyan <rediraph@mail.ru>
2022-02-11 11:02:30 +0700

    added two screens xmls

---
+++ app/src/main/java/com/r3d1r4ph/catsinfo/feature/cats/details/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.r3d1r4ph.catsinfo.feature.cats.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.r3d1r4ph.catsinfo.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
--- app/src/main/java/com/r3d1r4ph/catsinfo/feature/cats/list/ListActivity.kt
+++ app/src/main/java/com/r3d1r4ph/catsinfo/feature/cats/list/ListActivity.kt
@@ -1,11 +1,12 @@
-package com.r3d1r4ph.catsinfo
+package com.r3d1r4ph.catsinfo.feature.cats.list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.r3d1r4ph.catsinfo.R
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list)
     }
 }
